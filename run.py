955ed91cb79732f3d88acbeef507033238e019ea
pattrickx <pattrickx@edu.unifor.br>
2022-09-09 19:11:43 -0300

    port print

--- run.py
+++ run.py
@@ -3,12 +3,10 @@ from starlette.applications import Starlette
 from app import routes
 import os
 
-
-
+print("##### PORT: ",os.getenv("PORT"))
 app = Starlette(debug=True,
     routes = routes)
 
 if __name__=="__main__":
-    print("##### PORT: ",os.getenv("PORT"))
-    # uvicorn.run("run:app",workers=10,host='0.0.0.0',port=3000,log_level="debug",reload=True)
-    uvicorn.run("run:app",workers=10,host='0.0.0.0',port=os.getenv("PORT"),log_level="debug")
+    uvicorn.run("run:app",workers=10,host='0.0.0.0',port=5000,log_level="debug",reload=True)
+    # uvicorn.run("run:app",workers=10,host='0.0.0.0',port=5000,log_level="debug")
