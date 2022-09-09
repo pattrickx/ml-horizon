42ceefbc3696b5501b0eaa323b87e3e98babab82
pattrickx <pattrickx@edu.unifor.br>
2022-09-09 16:02:30 -0300

    update run.py

--- src/run.py
+++ src/run.py
@@ -8,5 +8,5 @@ app = Starlette(debug=True,
     routes = routes)
 
 if __name__=="__main__":
-    uvicorn.run("run:app",workers=10,host='0.0.0.0',port=5000,log_level="debug",reload=True)
-    # uvicorn.run("run:app",workers=10,host='0.0.0.0',port=5000,log_level="debug")
+    # uvicorn.run("run:app",workers=10,host='0.0.0.0',port=5000,log_level="debug",reload=True)
+    uvicorn.run("run:app",workers=10,host='0.0.0.0',port=5000,log_level="debug")
