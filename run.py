671e1cd6918f1cbafff9dd9446b6909910bffbf5
pattrickx <pattrickx@edu.unifor.br>
2022-09-09 20:18:08 -0300

    BASE_IRI

--- run.py
+++ run.py
@@ -9,7 +9,6 @@ app = Starlette(debug=True,
 
 if __name__=="__main__":
     print("##### PORT: ",os.getenv("PORT"))
-    print("##### HOST: ",os.getenv("HOST"))
-    print("##### host: ",os.getenv("host"))
+    print("##### BASE_IRI: ",os.getenv("BASE_IRI"))
     # uvicorn.run("run:app",workers=10,host='0.0.0.0',port=5000,log_level="debug",reload=True)
     uvicorn.run("run:app",workers=10,host=os.getenv("fwd"),port=os.getenv("PORT"),log_level="debug")
