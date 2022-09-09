70972ee593a9fb0d80b1ccade0f08cdd2330b3af
pattrickx <pattrickx@edu.unifor.br>
2022-09-09 20:13:30 -0300

    print HOST

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ app = Starlette(debug=True,
 
 if __name__=="__main__":
     print("##### PORT: ",os.getenv("PORT"))
-    print("##### PORT: ",os.getenv("HOST"))
-    print("##### PORT: ",os.getenv("host"))
+    print("##### HOST: ",os.getenv("HOST"))
+    print("##### host: ",os.getenv("host"))
     # uvicorn.run("run:app",workers=10,host='0.0.0.0',port=5000,log_level="debug",reload=True)
     uvicorn.run("run:app",workers=10,host=os.getenv("fwd"),port=os.getenv("PORT"),log_level="debug")
