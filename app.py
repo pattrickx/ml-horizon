90fb0379cbfef2e79328aec21742288d0005ec07
pattrickx <pattrickx@edu.unifor.br>
2022-09-09 17:05:41 -0300

    update test func

--- app.py
+++ app.py
@@ -23,9 +23,8 @@ async def summarize(request: Request) -> JSONResponse:
     return JSONResponse({"summarized_text":summy_text},status_code=200)
 
 async def test(request: Request) -> JSONResponse:
-    return JSONResponse({"test":"test"},status_code=200)
-routes =[ 
+    return JSONResponse({"Online":"OK"},status_code=200)
+routes =[ Route('/',endpoint=test,methods=["GET"]),
     Mount("/ai",routes=[
-        Route('/summarize',endpoint=summarize,methods=["GET"]),
-        Route('/test',endpoint=test,methods=["GET"])])
+        Route('/summarize',endpoint=summarize,methods=["GET"])])
     ]
