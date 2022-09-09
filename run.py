7c5174de09b19fe6f37dcf29b7f263f53efe7e3d
pattrickx <pattrickx@edu.unifor.br>
2022-09-09 20:31:17 -0300

    print hostname

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import uvicorn
 from starlette.applications import Starlette
 from app import routes
+import socket
 import os
 
 
@@ -9,6 +10,8 @@ app = Starlette(debug=True,
 
 if __name__=="__main__":
     print("##### PORT: ",os.getenv("PORT"))
-    print("##### BASE_IRI: ",os.getenv("BASE_IRI"))
+    host_name = socket.gethostname()
+    print(host_name )
+    print("##### BASE_IRI: ",socket.gethostbyname(host_name))
     # uvicorn.run("run:app",workers=10,host='0.0.0.0',port=5000,log_level="debug",reload=True)
-    uvicorn.run("run:app",workers=1,port=os.getenv("PORT"),log_level="debug")
+    uvicorn.run("run:app",host=socket.gethostbyname(host_name),workers=1,port=int(os.getenv("PORT")),log_level="debug")
