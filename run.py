b946d5b9b465cfa01fab78fb38d2d995240d2609
pattrickx <pattrickx@edu.unifor.br>
2022-09-09 18:28:25 -0300

    get port env

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import uvicorn
 from starlette.applications import Starlette
 from app import routes
+import os
 
 
 
@@ -8,5 +9,6 @@ app = Starlette(debug=True,
     routes = routes)
 
 if __name__=="__main__":
+    print("##### PORT: ",os.getenv("PORT"))
     # uvicorn.run("run:app",workers=10,host='0.0.0.0',port=3000,log_level="debug",reload=True)
-    uvicorn.run("run:app",workers=10,host='0.0.0.0',port=3000,log_level="debug")
+    uvicorn.run("run:app",workers=10,host='0.0.0.0',port=os.getenv("PORT"),log_level="debug")
