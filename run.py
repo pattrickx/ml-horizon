a57ee77ed668f679c4a3f5a53e84349231240794
pattrickx <pattrickx@edu.unifor.br>
2022-09-13 16:37:46 -0300

    disable cors

--- run.py
+++ run.py
@@ -4,9 +4,25 @@ from app import routes
 import socket
 import os
 
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
-app = Starlette(debug=True,
-    routes = routes)
+# from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+# from starlette.middleware.trustedhost import TrustedHostMiddleware
+
+# # Ensure that all requests include an 'example.com' or '*.example.com' host header,
+# # and strictly enforce https-only access.
+# middleware = [
+#   Middleware(TrustedHostMiddleware, allowed_hosts=['example.com', '*.example.com']),
+#   Middleware(HTTPSRedirectMiddleware)
+# ]
+
+
+middleware = [
+    Middleware(CORSMiddleware, allow_origins=['*'])
+]
+
+app = Starlette(debug=True, routes=routes, middleware=middleware)
 
 if __name__=="__main__":
     print("##### PORT: ",os.getenv("PORT"))
